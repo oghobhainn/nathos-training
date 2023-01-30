96dc3295f128843de74a85e8e4e753e59086324e
nathan verbruggen <nathanvbg@MacBook-Pro-de-nathan.local>
2023-01-30 22:01:56 +0100

    chap 1 -> 7

---
+++ estate/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Estate",
+    'depends': ['base'],
+    'application': True,
+    'license': 'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml',
+    ]   
+}
